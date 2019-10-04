da4e558a218be86e96a10fadcc26d0e21b917dd5
qilei123 <qileimail123@gmail.com>
2019-10-04 01:53:10 -0400

    all

--- models/models.py
+++ models/models.py
@@ -12,7 +12,7 @@ class SegmentationModuleBase(nn.Module):
 
     def pixel_acc(self, pred, label):
         _, preds = torch.max(pred, dim=1)
-        print(torch.max(pred))
+        print(torch.min(pred))
         valid = (label >= 0).long()
         #print(torch.max(valid))
         acc_sum = torch.sum(valid * (preds == label).long())
