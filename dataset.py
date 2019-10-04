7a1784e3d9df55673125fee3fc1cfac8d804cc17
qilei123 <qileimail123@gmail.com>
2019-10-04 00:59:27 -0400

    all

--- dataset.py
+++ dataset.py
@@ -360,7 +360,7 @@ class TrainROPRidgeDataset(BaseROPRidgeDataset):
     def __init__(self, root_dataset_,opt_,img_folder_="train2014",annotation_folder_ = "annotations",
                 anno_filename_="ridge_in_one_instances_train2014.json", batch_per_gpu=1, **kwargs):
         super(BaseROPRidgeDataset, self).__init__(root_dataset = root_dataset_,
-                opt=opt_,img_folder=img_folder_,annotation_folder=anno_filename_ ,
+                opt=opt_,img_folder=img_folder_,annotation_folder=annotation_folder_ ,
                 anno_filename=anno_filename_, **kwargs)
         #self.root_dataset = root_dataset
         # down sampling rate of segm labe
---
+++ train.sh
@@ -0,0 +1,2 @@
+git pull
+python3 train.py --gpus 0,1 --cfg config/ropridge-resnet50dilated-ppm_deepsup.yaml
