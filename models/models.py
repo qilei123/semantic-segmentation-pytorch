daaa8cae78a9a867d71e5147fbbd983218f13618
qilei123 <qileimail123@gmail.com>
2019-10-07 19:38:07 -0400

    all

--- models/models.py
+++ models/models.py
@@ -41,8 +41,9 @@ class SegmentationModule(SegmentationModuleBase):
             else:
                 pred = self.decoder(self.encoder(feed_dict['img_data'], return_feature_maps=True))
             showpred = pred.cpu().detach().numpy()
-            print(pred.size())
-            print(feed_dict['seg_label'].size())
+            print(torch.max(pred))
+            print(torch.min(pred))
+            #print(feed_dict['seg_label'].size())
             '''
             import cv2
             #cv2.imshow("test",showpred*255)
