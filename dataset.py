064a9a6137f54c848c1b0d05466247542276941a
qilei123 <qileimail123@gmail.com>
2019-10-07 23:37:05 -0400

    all

--- dataset.py
+++ dataset.py
@@ -344,9 +344,10 @@ class BaseROPRidgeDataset(torch.utils.data.Dataset):
     def img_transform(self, img):
         # 0-255 to 0-1
         img = np.float32(np.array(img)) / 255.
-        print(np.max(img))
+        
         img = img.transpose((2, 0, 1))
         img = self.normalize(torch.from_numpy(img.copy()))
+        print(torch.min(img))
         return img
 
     def segm_transform(self, segm):
