e216a03dae3c6a7870b10c0449c9b8d970533088
qilei123 <qileimail123@gmail.com>
2019-10-10 14:03:21 -0400

    all

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 git pull
-python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml
+python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_10.pth --result val10_res50
