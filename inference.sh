9b50f0b4ed212df3382715a380368ecddcab1388
qilei123 <qileimail123@gmail.com>
2019-10-14 20:31:25 -0400

    all

--- inference.sh
+++ inference.sh
@@ -1,2 +1,2 @@
 git pull
-python3 inference.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --image_path /data0/qilei_chen/Development/semantic-segmentation-pytorch/test/val_1775_8869ff5c4f272414f4b98046659df0cf.jpg --checkpoint epoch_25.pth --result test
+python3 inference.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --image_path /data0/qilei_chen/Development/semantic-segmentation-pytorch/test/val_1775_8869ff5c4f272414f4b98046659df0cf.jpg --checkpoint epoch_26.pth --result test
