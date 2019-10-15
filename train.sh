f4cadf6ad503111538070d196ddd552be93f4dbc
qilei123 <qileimail123@gmail.com>
2019-10-15 17:06:00 -0400

    all

--- train.sh
+++ train.sh
@@ -1,4 +1,5 @@
 git pull
 #python3 train.py --gpus 0,1 --cfg config/ropridge-resnet50dilated-ppm_deepsup.yaml
-python3 train.py --gpus 0,1 --cfg config/ropridge-resnet50-upernet.yaml
+#python3 train.py --gpus 0,1 --cfg config/ropridge-resnet50-upernet.yaml
 #python3 train.py --gpus 0,1 --cfg config/ropridge-resnet101-upernet.yaml
+python3 train.py --gpus 0,1 --cfg config/ropridge-hrnetv2.yaml
