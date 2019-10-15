2575ffb9556326ffabb97f71e5ea7097240eb726
qilei123 <qileimail123@gmail.com>
2019-10-14 21:28:53 -0400

    all

--- test.sh
+++ test.sh
@@ -8,10 +8,10 @@ python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint
 python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_23.pth --result val23_res50
 python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_24.pth --result val24_res50
 #python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_25.pth --result val25_res50
-python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_26.pth --result val26_res50
-python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_27.pth --result val27_res50
-python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_28.pth --result val28_res50
-python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_29.pth --result val29_res50
+#python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_26.pth --result val26_res50
+#python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_27.pth --result val27_res50
+#python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_28.pth --result val28_res50
+#python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_29.pth --result val29_res50
 #python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_30.pth --result val30_res50
 #python3 test.py --gpu 0 --cfg config/ropridge-resnet50-upernet.yaml --checkpoint epoch_35.pth --result val35_res50
 #python3 test.py --gpu 0 --cfg config/ropridge-resnet101-upernet.yaml --checkpoint epoch_5.pth --result val5_res101
