0708cab17775e12789697fa83b00e7fdcebb73ae
qilei123 <qileimail123@gmail.com>
2019-10-15 23:52:44 -0400

    all

--- count_ridges.py
+++ count_ridges.py
@@ -1,7 +1,7 @@
 from pycocotools.coco import COCO
 import os
 root_dataset = "/data0/qilei_chen/old_alien/AI_EYE_IMGS/ROP_DATASET_with_label/9LESIONS/"
-annotation_folder = "train2014"
+annotation_folder = "annotations"
 anno_filename = "ridge_in_one_instances_train2014.json"
 cocoAnno = COCO(os.path.join(root_dataset,annotation_folder,anno_filename))
 
