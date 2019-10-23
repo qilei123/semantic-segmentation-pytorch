b79d02f5f0cd685571d3426de5378db534c3db45
qilei123 <qileimail123@gmail.com>
2019-10-23 17:44:44 -0400

    all

--- test.py
+++ test.py
@@ -79,6 +79,7 @@ def test(segmentation_module, loader, gpu):
                 pred_tmp = segmentation_module(feed_dict, segSize=segSize)
                 scores = scores + pred_tmp / 1#len(cfg.DATASET.imgSizes)
             print(scores.size())
+            print(scores)
             _, pred = torch.max(scores, dim=1)
             pred = as_numpy(pred.squeeze(0).cpu())
         
